b9ce083756ca2a246ac0c1a1d80b402dc6d57a57
Yoonseok Chu <yoonseok.chu@gmail.com>
2020-05-23 09:18:25 +0900

    Recovering

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 print("Git...?")
 print("Yea! I Git it!")
-print("Did you Git it?")
-
-for i in range(3):
-    print("Yes! And we GitHub.")
 
 print("In this tutorial, we'll gonna talk about Machine Learning")
